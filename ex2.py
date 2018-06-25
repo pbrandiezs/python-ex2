389d199d921dac27a609976d7d6bb1c46e8ceae9
Perry Brandiezs <pbrandiezs@gmail.com>
2018-06-25 11:06:08 -0700

    First Commit

---
+++ ex2.py
@@ -0,0 +1,8 @@
+
+number = input("Enter a number? ")
+number = int(number)
+
+if (number % 2) == 0:
+    print(str(number) + " is an even number.")
+else:
+    print(str(number) + " is an odd number.")
